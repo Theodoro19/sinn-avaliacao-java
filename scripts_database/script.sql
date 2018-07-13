a7b57300f5d074b93cc60e29d32bbb6bd1ac07a2
Theodoro19 <raphaeltheodoro@hotmail.com>
2018-07-13 08:30:03 -0300

    Update script.sql

--- scripts_database/script.sql
+++ scripts_database/script.sql
@@ -1,3 +1,8 @@
+//SQL SERVER
+
+CREATE DATABASE sinn
+GO
+
 USE sinn
 CREATE TABLE empresa (codigo int not null IDENTITY(1,1) PRIMARY KEY, nome_empresa varchar(75), cnpj numeric(14,0), dono_empresa varchar(50))
 CREATE TABLE cargo(id int not null IDENTITY(1,1) PRIMARY KEY, nome_cargo varchar(20))
@@ -7,3 +12,19 @@ CREATE TABLE pessoa(codigo int not null IDENTITY(1,1) PRIMARY KEY, nome varchar(
 INSERT INTO sinn..cargo (nome_cargo) values ('Estagiário')
 INSERT INTO sinn..cargo (nome_cargo) values ('Gerente')
 INSERT INTO sinn..cargo (nome_cargo) values ('Diretor')
+
+//POSTGRESQL
+
+$ createdb sinn
+$ psql sinn
+CREATE TABLE empresa (codigo int PRIMARY KEY, nome_empresa varchar(75), 
+                      cnpj numeric(14,0), dono_empresa varchar(50));
+CREATE TABLE cargo(id int PRIMARY KEY, nome_cargo varchar(20));
+CREATE TABLE pessoa(codigo int PRIMARY KEY, nome varchar(50), cpf varchar(20)
+                    , salario_atual numeric(12,2), salario_reajuste numeric(12,2), empresa int 
+                    REFERENCES empresa(codigo), cargo int REFERENCES cargo (id));
+                    
+INSERT INTO sinn..cargo (nome_cargo) values ('Estagiário');
+INSERT INTO sinn..cargo (nome_cargo) values ('Gerente');
+INSERT INTO sinn..cargo (nome_cargo) values ('Diretor');
+
